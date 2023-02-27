da4bc74bee02dec630c2f2f468e9a1e799d73cb6
Cheuchheang Sear <searcheuchheang@gmail.com>
2023-02-28 00:21:56 +0700

    remove throw error

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,9 +8,8 @@ export const dashboardAction = async ({ request }) => {
   const data = await request.formData();
   const formData = Object.fromEntries(data);
   try {
-    // localStorage.setItem("userName", JSON.stringify(formData.userName));
-    // return toast.success(`Welcome, ${formData.userName}`);
-    throw new Error
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}`);
   } catch (error) {
     throw new Error("There was a problem creating your account.");
   }
