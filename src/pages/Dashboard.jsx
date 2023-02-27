5ca41750d1eb8a7235b56510b0b24204d7ae8001
Cheuchheang Sear <cheuchheang@ling-app.com>
2023-02-27 23:49:07 +0700

    add Intro & form creating new account

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
+import Intro from "../components/Intro";
 import { fetchData } from "../helpers";
+
+export const dashboardAction = async ({ request }) => {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}`);
+  } catch (error) {
+    throw new Error("There was a problem creating your account.");
+  }
+};
 
 export const dashboardLoader = () => {
   const userName = fetchData("userName");
@@ -9,7 +22,7 @@ export const dashboardLoader = () => {
 
 const Dashboard = () => {
   const { userName } = useLoaderData();
-  return <div>{userName}</div>;
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
 };
 
 export default Dashboard;
