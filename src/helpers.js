23f2fc53999ad4c93f76541ac299bde679004371
Cheuchheang Sear <searcheuchheang@gmail.com>
2023-03-14 23:48:43 +0700

    implement logic to handle form submit

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,26 @@
+const generateRandomColor = () => {
+  const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+  return `${existingBudgetLength*34} 65% 50%`
+};
+
 //local storage
 export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
+};
+
+export const createBudget = ({ name, amount }) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: Number(amount),
+    color: generateRandomColor(),
+  };
+  const existBudgets = fetchData("budgets") ?? [];
+  return localStorage.setItem(
+    "budgets",
+    JSON.stringify([...existBudgets, newItem])
+  );
 };
 
 export const deleteItem = ({ key }) => {
