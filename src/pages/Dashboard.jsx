05680ba574f7708e91677756d8b969ee164d9b03
Cheuchheang Sear <searcheuchheang@gmail.com>
2023-03-17 23:32:56 +0700

    add table for ExpenseItem

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
 import BudgetItem from "../components/BudgetItem";
 import Intro from "../components/Intro";
+import Table from "../components/Table";
 import { createBudget, createExpense, fetchData } from "../helpers";
 
 export const dashboardAction = async ({ request }) => {
@@ -48,11 +49,12 @@ export const dashboardAction = async ({ request }) => {
 export const dashboardLoader = () => {
   const userName = fetchData("userName");
   const budgets = fetchData("budgets");
-  return { userName, budgets };
+  const expenses = fetchData("expenses");
+  return { userName, budgets, expenses };
 };
 
 const Dashboard = () => {
-  const { userName, budgets } = useLoaderData();
+  const { userName, budgets, expenses } = useLoaderData();
   return (
     <>
       {userName ? (
@@ -69,12 +71,20 @@ const Dashboard = () => {
                 </div>
                 <h2>Existing Budgets</h2>
                 <div className="budgets">
-                  {
-                    budgets.map((budget)=>(
-                      <BudgetItem key={budget.id} budget={budget}/>
-                    ))
-                  }
+                  {budgets.map((budget) => (
+                    <BudgetItem key={budget.id} budget={budget} />
+                  ))}
                 </div>
+                {expenses && expenses.length > 0 && (
+                  <div className="grid-md">
+                    <h2>Recent Expenses</h2>
+                    <Table
+                      expenses={expenses.sort(
+                        (a, b) => b.createdAt - a.createdAt
+                      )}
+                    />
+                  </div>
+                )}
               </div>
             ) : (
               <div className="grid-sm">
